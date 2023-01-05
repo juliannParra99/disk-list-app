84ee7cebad940aa3a9b7c7d5807dab623443c81c
julianParrax <julianfranco.parra@gmail.com>
2023-01-04 21:29:49 -0300

    Se agrega clase accesoDatos para centralizar el acceso a datos

--- music-records/Negocio/DiscosNegocio.cs
+++ music-records/Negocio/DiscosNegocio.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
+using System.Data.SqlClient; 
 using Dominio;
                               
 namespace Negocio
@@ -14,43 +14,39 @@ namespace Negocio
         public List<Discos> listar()
         {
             List<Discos> lista = new List<Discos>();
-            SqlConnection conexion = new SqlConnection();
-            SqlCommand comando = new SqlCommand();
-            SqlDataReader lector;
-
+            AccesoDatos datos = new AccesoDatos();
+            
 
             try
             {
-                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=DISCOS_DB; integrated security=true"; 
-                comando.CommandType = System.Data.CommandType.Text; 
-                comando.CommandText = "SELECT D.Titulo,D.CantidadCanciones,D.UrlImagenTapa, E.Descripcion Estilo,TE.Descripcion Edicion FROM DISCOS D, TIPOSEDICION TE, ESTILOS E where D.IdEstilo = E.Id  AND D.IdTipoEdicion = TE.Id"; 
-                comando.Connection = conexion;
+                datos.setearConsulta("SELECT D.Titulo,D.CantidadCanciones,D.UrlImagenTapa, E.Descripcion Estilo,TE.Descripcion Edicion FROM DISCOS D, TIPOSEDICION TE, ESTILOS E where D.IdEstilo = E.Id  AND D.IdTipoEdicion = TE.Id");
+                datos.ejecutarLectura();
 
-                conexion.Open();
-                lector = comando.ExecuteReader(); 
-
-                while (lector.Read()) 
+                while (datos.Lector.Read()) 
                 {
                     Discos aux = new Discos(); 
-                    aux.Titulo = lector.GetString(0); 
-                    aux.CantidadCanciones = (int)lector["CantidadCanciones"]; 
-                    aux.UrlImagen = (string)lector["UrlImagenTapa"];
+                    aux.Titulo = datos.Lector.GetString(0); 
+                    aux.CantidadCanciones = (int)datos.Lector["CantidadCanciones"]; 
+                    aux.UrlImagen = (string)datos.Lector["UrlImagenTapa"];
                     aux.Estilo = new Estilo(); 
-                    aux.Estilo.Estilo_disco = (string)lector["Estilo"];
+                    aux.Estilo.Estilo_disco = (string)datos.Lector["Estilo"];
                     aux.Edicion = new Edicion();
-                    aux.Edicion.Edicion_disco = (string)lector["Edicion"];
+                    aux.Edicion.Edicion_disco = (string)datos.Lector["Edicion"];
         
 
                     lista.Add(aux); 
                     
                 }
-                conexion.Close(); 
                 return lista; 
             }
             catch (Exception ex)
             {
 
                 throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
             }
         }
     }
